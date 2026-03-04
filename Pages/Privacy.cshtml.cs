b5a49cbc57c26b58a0b466e478211fa662c413d8
Mykie <mykie_chen@hotmail.ca>
2026-03-04 13:40:04 -0500

    done

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mslearn_create_razor_pages_aspnet_core.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
